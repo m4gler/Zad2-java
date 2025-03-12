fd6eeb540a1d2d9ed0c8b640ebcdc8d1c3b27aac
m4gler <przedosiaczek@gmail.com>
2025-03-12 20:36:58 +0100

    komit

--- src/EmailProvider.java
+++ src/EmailProvider.java
@@ -1,3 +1,9 @@
 public class EmailProvider implements NotificationProvider {
-    
+    @Override
+    public void send(String message, User user) {
+        if(user.getIsEmailNotificationOn()) {
+
+        }
+
+    }
 }
